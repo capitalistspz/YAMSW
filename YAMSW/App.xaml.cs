2cbd1a482736e484eaff7611d12812fa1748037e
capitalistspz <amponsah735@gmail.com>
2021-01-04 21:02:15 +0000

    Add project files.

---
+++ YAMSW/App.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace YAMSW
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string serverFileName = System.IO.Directory.GetCurrentDirectory() + @"\server.jar";
+        public static int minMemory = 1024;
+        public static int maxMemory = 2048;
+        public static int instanceNo = 0;
+        public static char unit = 'M';
+        public static bool hasNewInput = false;
+        public static bool hasNewOutput = false;
+        public static string newInput;
+        public static bool isRunning;
+        public static string lastOut;
+        public static TextBlock outputTb;
+        public static bool programOpen;
+
+        public static void startServer() {
+
+            System.Diagnostics.Process serverProcess = new System.Diagnostics.Process();
+            
+            serverProcess.StartInfo.FileName = "java";
+            serverProcess.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(serverFileName);
+            serverProcess.StartInfo.Arguments = "-Xms" + minMemory + unit + " -Xmx" + maxMemory + unit + " -jar " + serverFileName + " -nogui";
+            serverProcess.StartInfo.UseShellExecute = false;
+            serverProcess.StartInfo.RedirectStandardOutput = true;
+            serverProcess.StartInfo.RedirectStandardInput = true;
+            serverProcess.StartInfo.CreateNoWindow = true;
+
+            serverProcess.Start();
+            isRunning = true;
+            programOpen = true;
+            
+
+            Thread inputThread = new Thread(new ParameterizedThreadStart(inputWrite));
+            inputThread.Start(serverProcess.StandardInput);
+            Thread outputThread = new Thread(new ParameterizedThreadStart(outputRead));
+            outputThread.Start(serverProcess.StandardOutput);
+
+            inputThread.Join();
+            outputThread.Join();
+            serverProcess.WaitForExit();
+            programOpen = false;
+
+
+
+
+
+        }
+        public static void inputWrite(object streamWriter) {
+            System.IO.StreamWriter writer = (System.IO.StreamWriter)streamWriter;
+            while (isRunning) {
+                if (hasNewInput) {
+                    writer.WriteLine(newInput);
+                    hasNewInput = false;
+                    if (newInput == "stop")
+                        break;
+                }
+            }
+            writer.WriteLine (" ");
+            
+        }
+        public static void outputRead(object streamReader)
+        {
+            System.IO.StreamReader reader = (System.IO.StreamReader)streamReader;
+            string temp;
+            while (isRunning)
+            {
+                temp = reader.ReadLine();
+                if (temp != null)
+                {
+                    lastOut = temp;
+
+                    YAMSW.MainWindow.Instances[instanceNo].Dispatcher.BeginInvoke(new ThreadStart(() => YAMSW.MainWindow.Instances[instanceNo].CommandLineTextBlock.Text += temp));
+                }
+
+            }
+
+        }
+        public ServerInfo parseServerInfo(string str)
+        {
+            Regex regex = new Regex(@"\[([0-9]{2}:[0-9]{2}:[0-9]{2})\] \[([\w-]*)\/([A-Z]*)\]: (.*)", RegexOptions.Compiled);
+            MatchCollection matches = regex.Matches(str);
+            GroupCollection groups = matches[0].Groups;
+            return new ServerInfo(groups[0].Value, groups[1].Value, groups[2].Value, groups[3].Value);
+        }
+        public class ServerInfo
+        {
+            public string time { get; private set; }
+            public string thread { get; private set; }
+            public string logLevel { get; private set; }
+            public string feedback { get; private set; }
+            public ServerInfo(string time, string thread, string logLevel, string feedback)
+            {
+                this.time = time;
+                this.thread = thread;
+                this.logLevel = logLevel;
+                this.feedback = feedback;
+            }
+        }
+    }
+}
---
+++ YAMSW/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YAMSW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<MainWindow> Instances = new List<MainWindow>();
+        public static int instanceCount = 0;
+        private int instanceNo;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instances.Add(this);
+            instanceNo = instanceCount++;
+        }
+
+        private void fileLocationBox_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void serverStartButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button startButton = (Button)sender;
+            if (App.isRunning)
+            {
+                startButton.Content = "Stopping server...";
+                startButton.IsEnabled = false;
+
+                App.hasNewInput = true;
+                App.newInput = "stop";
+
+                while (App.isRunning) {
+                }
+
+                startButton.Content = "Start server";
+            }
+            else
+            {
+                startButton.Content = "Starting server...";
+                startButton.IsEnabled = false;
+                this.CommandLineTextBlock.Text = "\0";
+                App.outputTb = CommandLineTextBlock;
+                if (fileLocationBox.Text != null) {
+                    App.serverFileName = fileLocationBox.Text;
+                }
+                else
+                {
+                    App.serverFileName = @"\server.jar";
+                }
+                if (Int32.TryParse(minRAMTextBox.Text, out App.minMemory) && Int32.TryParse(maxRAMTextBox.Text, out App.maxMemory)) 
+                {
+                    if (MemButton_1.IsChecked == true)
+                        App.unit = '\0';
+
+                    else if (MemButton_3.IsChecked == true)
+                        App.unit = 'G';
+
+                    else
+                        App.unit = 'M';
+                }
+                else
+                {
+                    App.minMemory = 1024;
+                    App.maxMemory = 1024;
+                    App.unit = 'M';
+                }
+                App.instanceNo = this.instanceNo;
+
+                App.startServer();
+                startButton.Content = "Stop server";
+            }
+        }
+        public void updateOutputBox(string str)
+        {
+            CommandLineTextBlock.Text += str;
+        }
+        ~MainWindow()
+        {
+            --instanceCount;
+        }
+
+        private void fileLocationButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Jar files (*.jar)|*.jar|All files (*.*)|*.*",
+                InitialDirectory = System.IO.Directory.GetCurrentDirectory(),
+                Multiselect = false
+            };
+            if (openFileDialog.ShowDialog() == true)
+                fileLocationBox.Text = openFileDialog.FileName;
+        }
+
+        private int[] remSpaces(string str) {
+            List <int> pos = new List<int>();
+            Regex regex = new Regex(@"\s");
+            MatchCollection matches = regex.Matches(str);
+            int count = matches.Count;
+            int it = 0;
+            while (it < count)
+            {
+                str.Remove(matches[it].Index);
+                --it; --count;
+            }
+
+            return pos.ToArray();
+        }
+
+    }
+}
